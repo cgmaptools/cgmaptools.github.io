38883185f59e26f99cb5db0ce1c71d64b1d4c4d9
Ping Zhu <pingzhu.work@gmail.com>
2017-01-13 20:52:30 +0800

    Site updated: 2017-01-13 20:52:30

--- cgmaptools_documentation/libs/gitbook-2.6.7/js/plugin-bookdown.js
+++ cgmaptools_documentation/libs/gitbook-2.6.7/js/plugin-bookdown.js
@@ -106,7 +106,39 @@ require(["gitbook", "lodash", "jQuery"], function(gitbook, _, $) {
     if (toc && toc.scroll_highlight !== false) (function() {
       // scroll the current TOC item into viewport
       var ht = $(window).height(), rect = li[0].getBoundingClientRect();
-      if (rect.top >= ht || rect.top <= 0="" ||="" rect.bottom="" <="0)" {="" summary.scrolltop(li[0].offsettop);="" }="" current="" chapter="" toc="" items="" var="" +="" href="" '"]').parent('li.chapter'),="" m="items.length;" if="" (m="==" 0)="" return;="" all="" section="" titles="" on="" page="" hs="bookInner.find('.page-inner').find('h1,h2,h3')," n="hs.length," ts="hs.map(function(i," el)="" return="" $(el).text();="" });="" (n="==" scrollhandler="function(e)" ht="$(window).height();" cleartimeout($.data(this,="" 'scrolltimer'));="" $.data(this,="" 'scrolltimer',="" settimeout(function()="" find="" the="" first="" visible="" title="" in="" viewport="" for="" (var="" i="0;" n;="" i++)="" rect="hs[i].getBoundingClientRect();" (rect.top="">= 0 && rect.bottom <= ht)="" break;="" }="" if="" (i="==" n)="" return;="" items.removeclass('active');="" for="" (var="" j="0;" <="" m;="" j++)="" {="" (items.eq(j).children('a').first().text()="==" ts[i])="" (j="==" m)="" highlight="" the="" chapter="" title="" search="" bottom-up="" a="" visible="" toc="" item="" to="" highlight;="" an="" is="" hidden,="" we="" check="" its="" parent="" visible,="" and="" so="" on="" while=""> 0 && items.eq(j).is(':hidden')) j--;
+      if (rect.top >= ht || rect.top <= 0 || rect.bottom <= 0) {
+        summary.scrollTop(li[0].offsetTop);
+      }
+      // current chapter TOC items
+      var items = $('a[href^="' + href + '"]').parent('li.chapter'),
+          m = items.length;
+      if (m === 0) {
+        items = summary.find('li.chapter');
+        m = items.length;
+      }
+      if (m === 0) return;
+      // all section titles on current page
+      var hs = bookInner.find('.page-inner').find('h1,h2,h3'), n = hs.length,
+          ts = hs.map(function(i, el) { return $(el).text(); });
+      if (n === 0) return;
+      var scrollHandler = function(e) {
+        var ht = $(window).height();
+        clearTimeout($.data(this, 'scrollTimer'));
+        $.data(this, 'scrollTimer', setTimeout(function() {
+          // find the first visible title in the viewport
+          for (var i = 0; i < n; i++) {
+            var rect = hs[i].getBoundingClientRect();
+            if (rect.top >= 0 && rect.bottom <= ht) break;
+          }
+          if (i === n) return;
+          items.removeClass('active');
+          for (var j = 0; j < m; j++) {
+            if (items.eq(j).children('a').first().text() === ts[i]) break;
+          }
+          if (j === m) j = 0;  // highlight the chapter title
+          // search bottom-up for a visible TOC item to highlight; if an item is
+          // hidden, we check if its parent is visible, and so on
+          while (j > 0 && items.eq(j).is(':hidden')) j--;
           items.eq(j).addClass('active');
         }, 250));
       };
@@ -178,4 +210,3 @@ require(["gitbook", "lodash", "jQuery"], function(gitbook, _, $) {
   });
 
 });
-</=></=></a>
